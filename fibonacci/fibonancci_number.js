6e91a910dbf4eef7b86063f6db236906343744d7
duc minh <21ducminh@gmail.com>
2024-02-29 16:22:00 +0700

    fibonancci

---
+++ fibonacci/fibonancci_number.js
@@ -0,0 +1,17 @@
+function fibonancci(n) {
+    if (n <= 1) {
+        return n;
+    }
+
+    let prev1 = 0;
+    let prev2 = 1;
+    let result = 0;
+
+    for (let i = 2; i < n; i++) {
+        result = prev2 + prev1;
+        prev1 = prev2;
+        prev2 = result
+    }
+
+    return result;
+}
