d6ad0d260e07aff10935d860cb3a0f9e93224b00
duc minh <21ducminh@gmail.com>
2024-03-02 15:57:11 +0700

    isPrime

--- fibonacci/fibonancci_number.js
+++ fibonacci/fibonancci_number.js
@@ -7,7 +7,7 @@ function fibonancci(n) {
     let prev2 = 1;
     let result = 0;
 
-    for (let i = 2; i < n; i++) {
+    for (let i = 2; i <= n; i++) {
         result = prev2 + prev1;
         prev1 = prev2;
         prev2 = result
